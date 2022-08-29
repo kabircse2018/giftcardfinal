a4c5ed2d643f118bf666cb738f3a4abc5f59c5c1
kabircse2018 <kabircse2018@gmail.com>
2022-08-29 18:12:38 +0600

    Outlet Store

--- app/Http/Controllers/OutletController.php
+++ app/Http/Controllers/OutletController.php
@@ -20,7 +20,7 @@ class OutletController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
+     * Show the form for creating a new resource. 
      *
      * @return \Illuminate\Http\Response
      */
